8aa1f5a5c786a799faccfda6b8eb62382286d03a
JurassicBeast <chilerdnokuk@gmail.com>
2024-02-18 02:43:27 +0200

    Version: 0.0.6 In the "AngerCommand" class: - Changed the literal parameter of the anger command "set" to "add". - Added a new optional parameter (anger_time) at the end of the command with the "set" branch, it's used to tell for how long an entity is going to be angry for (currently works for some entities). - Changed the name of the "setAnger" method to "addAnger". - In the: "addAnger" method, the expiry of MemoryModuleType is no longer hard coded. - In the: "removeAnger" added a new variable(Optional<LivingEntity) to store the targets of the warden entity.

--- src/main/java/net/angercommand/AngerCommand.java
+++ src/main/java/net/angercommand/AngerCommand.java
@@ -1,76 +1,95 @@
 package net.angercommand;
 
 import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
-import net.minecraft.entity.mob.*;
-import net.minecraft.entity.passive.*;
-import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.WardenEntity;
+import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.passive.PolarBearEntity;
+import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 
 public class AngerCommand {
-    private static Collection<? extends Entity> global_targetEntities = null;
-
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("anger").requires(source -> source.hasPermissionLevel(2))
-                .then(literal("set")
+                .then(literal("add")
                         .then(argument("attacker(s)", EntityArgumentType.entities())
                                 .then(argument("target(s)", EntityArgumentType.entities())
-                                        .executes(context -> setAnger(context, EntityArgumentType.getEntities(context, "attacker(s)"), EntityArgumentType.getEntities(context, "target(s)"), false))
+                                        .executes(context -> addAnger(context, EntityArgumentType.getEntities(context, "attacker(s)"), EntityArgumentType.getEntities(context, "target(s)"), false, 30))
                                         .then(argument("shuffle_targets", BoolArgumentType.bool())
-                                                .executes(context -> setAnger(context, EntityArgumentType.getEntities(context, "attacker(s)"), EntityArgumentType.getEntities(context, "target(s)"), BoolArgumentType.getBool(context, "shuffle_targets")))))))
+                                                .executes(context -> addAnger(context, EntityArgumentType.getEntities(context, "attacker(s)"), EntityArgumentType.getEntities(context, "target(s)"), BoolArgumentType.getBool(context, "shuffle_targets"), 30))
+                                                .then(argument("anger_time", IntegerArgumentType.integer(0))
+                                                        .executes(context -> addAnger(context, EntityArgumentType.getEntities(context, "attacker(s)"), EntityArgumentType.getEntities(context, "target(s)"), BoolArgumentType.getBool(context, "shuffle_targets"), IntegerArgumentType.getInteger(context, "anger_time"))))))))
                 .then(literal("clear")
                         .then(argument("attacker(s)", EntityArgumentType.entities())
-                                .executes(ctx -> removeAnger(ctx, EntityArgumentType.getEntities(ctx, "attacker(s)")))))));
+                                .executes(context -> removeAnger(context, EntityArgumentType.getEntities(context, "attacker(s)")))))));
     }
 
-    public static int setAnger(CommandContext<ServerCommandSource> ctx, Collection<? extends Entity> attackerEntities, Collection<? extends Entity> targetEntities, boolean shuffleTargets) {
+    public static int addAnger(CommandContext<ServerCommandSource> context, Collection<? extends Entity> attackerEntities, Collection<? extends Entity> targetEntities, boolean shuffleTargets, int angerTime) {
         if (attackerEntities.size() == 1 && !(attackerEntities.iterator().next() instanceof MobEntity)) {
-            ctx.getSource().sendError(Text.of("Unable to set attacker(s)"));
+            context.getSource().sendError(Text.of("Unable to set attacker(s)"));
             return 0;
         }
 
         if (targetEntities.size() == 1 && !(targetEntities.iterator().next() instanceof LivingEntity)) {
-            ctx.getSource().sendError(Text.of("Unable to set target(s)"));
+            context.getSource().sendError(Text.of("Unable to set target(s)"));
             return 0;
         }
 
         int angeredEntitiesCount = 0;
         int targetEntitiesCount = 0;
-        global_targetEntities = targetEntities;
         List<? extends Entity> targetEntitiesList = new ArrayList<>(targetEntities);
 
         for (Entity attackerEntity : attackerEntities) {
-            if (attackerEntity instanceof MobEntity attacker && EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR.test(attacker)) {
+            if (attackerEntity instanceof MobEntity attacker) {
                 if (shuffleTargets && attackerEntities.size() > 1) {
                     Collections.shuffle(targetEntitiesList);
                 }
 
                 for (Entity targetEntity : targetEntitiesList) {
-                    if (targetEntity instanceof LivingEntity target && !attacker.equals(targetEntity) && EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR.test(target)) {
+                    if (targetEntity instanceof LivingEntity target && !attacker.equals(targetEntity)) {
                         attacker.setTarget(target);
-                        attacker.setAttacking(true);
-                        attacker.getBrain().remember(MemoryModuleType.ANGRY_AT, target.getUuid(), 600L);
-                        attacker.getBrain().remember(MemoryModuleType.ATTACK_TARGET, target, 600L);
+                        attacker.getBrain().remember(MemoryModuleType.ANGRY_AT, target.getUuid(), angerTime * 20L);
+                        attacker.getBrain().remember(MemoryModuleType.ATTACK_TARGET, target, angerTime * 20L);
 
-                        if (attacker instanceof WardenEntity warden) {
-                            warden.increaseAngerAt(target, 150, false);
+                        if (attacker instanceof BeeEntity beeEntity) {
+                            beeEntity.setAngryAt(targetEntity.getUuid());
+                            beeEntity.setAngerTime(angerTime * 20);
+                        } else if (attacker instanceof EndermanEntity endermanEntity) {
+                            endermanEntity.setAngryAt(targetEntity.getUuid());
+                            endermanEntity.setAngerTime(angerTime * 20);
+                        } else if (attacker instanceof IronGolemEntity ironGolemEntity) {
+                            ironGolemEntity.setAngryAt(targetEntity.getUuid());
+                            ironGolemEntity.setAngerTime(angerTime * 20);
+                        } else if (attacker instanceof PolarBearEntity polarBearEntity) {
+                            polarBearEntity.setAngryAt(targetEntity.getUuid());
+                            polarBearEntity.setAngerTime(angerTime * 20);
+                        } else if (attacker instanceof WardenEntity warden) {
+                            warden.increaseAngerAt(target, angerTime * 20, false);
+                        } else if (attacker instanceof WolfEntity wolfEntity) {
+                            wolfEntity.setAngryAt(targetEntity.getUuid());
+                            wolfEntity.setAngerTime(angerTime * 20);
+                        } else if (attacker instanceof ZombifiedPiglinEntity zombifiedPiglinEntity) {
+                            zombifiedPiglinEntity.setAngryAt(targetEntity.getUuid());
+                            zombifiedPiglinEntity.setAngerTime(angerTime * 20);
                         }
+
                         targetEntitiesCount++;
                     }
                 }
@@ -79,81 +98,71 @@ public class AngerCommand {
         }
 
         if (angeredEntitiesCount == 0 || targetEntitiesCount == 0) {
-            ctx.getSource().sendError(Text.of("Unable to set attacker(s) nor target(s)"));
+            context.getSource().sendError(Text.of("Unable to set attacker(s) nor target(s)"));
             return 0;
         }
 
         targetEntitiesCount = targetEntitiesCount / angeredEntitiesCount;
 
-        int finalAngeredEntitiesCount = angeredEntitiesCount;
-        int finalTargetEntitiesCount = targetEntitiesCount;
-
         if (angeredEntitiesCount > 1) {
+            int finalAngeredEntitiesCount = angeredEntitiesCount;
             if (targetEntitiesCount > 1) {
-                ctx.getSource().sendFeedback(() -> Text.translatable("Angered %s entities at %s entities", finalAngeredEntitiesCount, targetEntities.size()), true);
+                context.getSource().sendFeedback(() -> Text.translatable("Angered %s entities at %s entities", finalAngeredEntitiesCount, targetEntities.size()), true);
             } else {
-                ctx.getSource().sendFeedback(() -> Text.translatable("Angered %s entities at %s", finalAngeredEntitiesCount, targetEntities.iterator().next().getDisplayName()), true);
+                context.getSource().sendFeedback(() -> Text.translatable("Angered %s entities at %s", finalAngeredEntitiesCount, targetEntities.iterator().next().getDisplayName()), true);
             }
         } else {
             if (targetEntitiesCount > 1) {
-                ctx.getSource().sendFeedback(() -> Text.translatable("Angered %s at %s entities", attackerEntities.iterator().next().getDisplayName(), finalTargetEntitiesCount), true);
+                int finalTargetEntitiesCount = targetEntitiesCount;
+                context.getSource().sendFeedback(() -> Text.translatable("Angered %s at %s entities", attackerEntities.iterator().next().getDisplayName(), finalTargetEntitiesCount), true);
             } else {
-                ctx.getSource().sendFeedback(() -> Text.translatable("Angered %s at %s", attackerEntities.iterator().next().getDisplayName(), targetEntities.iterator().next().getDisplayName()), true);
+                context.getSource().sendFeedback(() -> Text.translatable("Angered %s at %s", attackerEntities.iterator().next().getDisplayName(), targetEntities.iterator().next().getDisplayName()), true);
             }
         }
-
         return 1;
     }
 
-    public static int removeAnger(CommandContext<ServerCommandSource> ctx, Collection<? extends Entity> attackerEntities) {
-        if (global_targetEntities == null) {
-            if (attackerEntities.size() == 1) {
-                ctx.getSource().sendError(Text.of("Unable to remove the anger of the attacker because it didn't have target(s)"));
-            } else {
-                ctx.getSource().sendError(Text.of("Unable to remove the anger of the attackers because they didn't have target(s)"));
-            }
-            return 0;
-        }
-
+    public static int removeAnger(CommandContext<ServerCommandSource> context, Collection<? extends Entity> attackerEntities) {
         int calmedEntities = 0;
+        Optional<LivingEntity> target;
 
         for (Entity attackerEntity : attackerEntities) {
-            if (attackerEntity instanceof MobEntity attacker && EntityPredicates.EXCEPT_CREATIVE_OR_SPECTATOR.test(attacker)) {
-                for (Entity targetEntity : global_targetEntities) {
-                    if (targetEntity instanceof LivingEntity target) {
-                        attacker.setTarget(null);
-                        attacker.getBrain().forget(MemoryModuleType.ANGRY_AT);
-                        attacker.getBrain().forget(MemoryModuleType.ATTACK_TARGET);
+            if (attackerEntity instanceof MobEntity attacker) {
+                attacker.setTarget(null);
+                attacker.getBrain().forget(MemoryModuleType.ANGRY_AT);
+                attacker.getBrain().forget(MemoryModuleType.ATTACK_TARGET);
 
-                        if (attacker instanceof BeeEntity beeEntity) {
-                            beeEntity.stopAnger();
-                        } else if (attacker instanceof EndermanEntity endermanEntity) {
-                            endermanEntity.stopAnger();
-                        } else if (attacker instanceof IronGolemEntity ironGolemEntity) {
-                            ironGolemEntity.stopAnger();
-                        } else if (attacker instanceof PolarBearEntity polarBearEntity) {
-                            polarBearEntity.stopAnger();
-                        } else if (attacker instanceof WardenEntity warden) {
-                            warden.removeSuspect(target);
-                        } else if (attacker instanceof WolfEntity wolfEntity) {
-                            wolfEntity.stopAnger();
-                        } else if (attacker instanceof ZombifiedPiglinEntity zombifiedPiglinEntity) {
-                            zombifiedPiglinEntity.stopAnger();
-                        }
+                if (attacker instanceof BeeEntity beeEntity) {
+                    beeEntity.stopAnger();
+                } else if (attacker instanceof EndermanEntity endermanEntity) {
+                    endermanEntity.stopAnger();
+                } else if (attacker instanceof IronGolemEntity ironGolemEntity) {
+                    ironGolemEntity.stopAnger();
+                } else if (attacker instanceof PolarBearEntity polarBearEntity) {
+                    polarBearEntity.stopAnger();
+                } else if (attacker instanceof WardenEntity warden) {
+                    target = warden.getPrimeSuspect();
+                    while (target.isPresent()) {
+                        warden.removeSuspect(target.get());
+                        target = warden.getPrimeSuspect();
                     }
+                } else if (attacker instanceof WolfEntity wolfEntity) {
+                    wolfEntity.stopAnger();
+                } else if (attacker instanceof ZombifiedPiglinEntity zombifiedPiglinEntity) {
+                    zombifiedPiglinEntity.stopAnger();
                 }
                 calmedEntities++;
             }
         }
 
         if (calmedEntities == 0) {
-            ctx.getSource().sendError(Text.of("Unable to remove the target(s) of the attacker(s)"));
+            context.getSource().sendError(Text.of("Unable to remove the target(s) of the attacker(s)"));
             return 0;
         } else if (calmedEntities == 1) {
-            ctx.getSource().sendFeedback(() -> Text.translatable("Successfully removed the target(s) of %s", attackerEntities.iterator().next().getDisplayName()), true);
+            context.getSource().sendFeedback(() -> Text.translatable("Successfully removed the target(s) of %s", attackerEntities.iterator().next().getDisplayName()), true);
         } else {
             int finalCalmedEntities = calmedEntities;
-            ctx.getSource().sendFeedback(() -> Text.translatable("Successfully removed the target(s) of %s attackers", finalCalmedEntities), true);
+            context.getSource().sendFeedback(() -> Text.translatable("Successfully removed the target(s) of %s attackers", finalCalmedEntities), true);
         }
 
         return 1;
